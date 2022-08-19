b2a0892a05cf44abaf9114d9de51bd63430d9635
dalfredi <dalfredi@student.21-school.ru>
2022-08-19 12:15:34 +0300

    merge

--- src/main/java/edu/school21/bots/passbot/basicui/commands/StartCommand.java
+++ src/main/java/edu/school21/bots/passbot/basicui/commands/StartCommand.java
@@ -1,7 +1,5 @@
 package edu.school21.bots.passbot.basicui.commands;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.school21.bots.passbot.basicui.commands.meta.Command;
 import edu.school21.bots.passbot.basicui.commands.meta.CommandWithArguments;
 import edu.school21.bots.passbot.dal.models.User;
@@ -11,11 +9,8 @@ import edu.school21.bots.passbot.mail.NotificationService;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.ArrayList;
@@ -33,10 +28,12 @@ public class StartCommand implements CommandWithArguments, Command {
     private final String name = "/start";
     private final Integer maxArgs = 4;
     private final Map<Integer, String> prompts = new HashMap<>();
+    private boolean error;
     private Long chatId;
     private Integer currentStep;
     private List<String> arguments = new ArrayList<>();
     private final UserService userService;
+    private String responseText;
 
     public StartCommand(UserService userService,
                         NotificationService notificationService,
@@ -79,7 +76,7 @@ public class StartCommand implements CommandWithArguments, Command {
                 return;
             }
             try {
-                user = requestAccessToken(argument);
+                user = apiService.requestAccessToken(argument);
             } catch (Exception e) {
                 e.getMessage();
             }
@@ -127,44 +124,44 @@ public class StartCommand implements CommandWithArguments, Command {
         return response;
     }
 
-    private User requestAccessToken(String name) throws IOException {
-        ResponseEntity<String> response;
-        User user = new User();
-
-        RestTemplate restTemplate = new RestTemplate();
-        String clientId = credentialId + ":" + clientSecret;
-        String encodedCredentials = new String(Base64.encodeBase64(clientId.getBytes()));
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("Authorization", "Basic " + encodedCredentials);
-
-        HttpEntity<String> request = new HttpEntity<String>(headers);
-        String access_token_url = "https://api.intra.42.fr/oauth/token";
-        access_token_url += "?code=" + clientId;
-        access_token_url += "&grant_type=client_credentials";
-
-        response = restTemplate.exchange(access_token_url, HttpMethod.POST, request, String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = mapper.readTree(response.getBody());
-        String token = node.path("access_token").asText();
-
-        String url = "https://api.intra.42.fr/v2/users?filter[login]=" + name;
-        HttpHeaders headers1 = new HttpHeaders();
-        headers1.add("Authorization", "Bearer " + token);
-        HttpEntity<String> entity = new HttpEntity<>(headers1);
-
-        ResponseEntity<String> peers = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        JsonNode node1 = mapper.readTree(peers.getBody());
-        Iterator<Map.Entry<String, JsonNode>> node2 = node1.path(0).fields();
-        while (node2.hasNext()) {
-            Map.Entry<String, JsonNode> tmp = node2.next();
-            if (tmp.getKey().equals("login")) {
-                user.setLogin(tmp.getValue().asText());
-            }
-            if (tmp.getKey().equals("staff?")) {
-                user.setRole(tmp.getValue().asText().equals("false") ? "USER" : "ADMIN");
-            }
-        }
-        return user;
-    }
+//    private User requestAccessToken(String name) throws IOException {
+//        ResponseEntity<String> response;
+//        User user = new User();
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        String clientId = credentialId + ":" + clientSecret;
+//        String encodedCredentials = new String(Base64.encodeBase64(clientId.getBytes()));
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//        headers.add("Authorization", "Basic " + encodedCredentials);
+//
+//        HttpEntity<String> request = new HttpEntity<String>(headers);
+//        String access_token_url = "https://api.intra.42.fr/oauth/token";
+//        access_token_url += "?code=" + clientId;
+//        access_token_url += "&grant_type=client_credentials";
+//
+//        response = restTemplate.exchange(access_token_url, HttpMethod.POST, request, String.class);
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode node = mapper.readTree(response.getBody());
+//        String token = node.path("access_token").asText();
+//
+//        String url = "https://api.intra.42.fr/v2/users?filter[login]=" + name;
+//        HttpHeaders headers1 = new HttpHeaders();
+//        headers1.add("Authorization", "Bearer " + token);
+//        HttpEntity<String> entity = new HttpEntity<>(headers1);
+//
+//        ResponseEntity<String> peers = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+//        JsonNode node1 = mapper.readTree(peers.getBody());
+//        Iterator<Map.Entry<String, JsonNode>> node2 = node1.path(0).fields();
+//        while (node2.hasNext()) {
+//            Map.Entry<String, JsonNode> tmp = node2.next();
+//            if (tmp.getKey().equals("login")) {
+//                user.setLogin(tmp.getValue().asText());
+//            }
+//            if (tmp.getKey().equals("staff?")) {
+//                user.setRole(tmp.getValue().asText().equals("false") ? "USER" : "ADMIN");
+//            }
+//        }
+//        return user;
+//    }
 }
